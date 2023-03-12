991f861886e957779de89281f1321ce00d784487
Darien Maynard <darienmaynard@gmail.com>
2023-03-12 16:15:10 -0500

    update 1

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
 #include <Arduino.h>
+#include <Wifi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+#include "env.h" 
+
+#define endpoint "rg-lab6-api.onrender.com"
+
+#define fanPin 22
+#define lightPin 23
 
 void setup() {
   // put your setup code here, to run once:
